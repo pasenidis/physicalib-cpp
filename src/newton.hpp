8d42f3bf24426dd72810f064059b73d092c69681
Edward Pasenidis <me@edpasenidis.tech>
2020-06-15 02:13:21 +0300

    feat: implement femt, third law & univ. gravity
    needs some work

---
+++ src/newton.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+namespace LibPhysica 
+{
+    double femt(double mass, std::string planet);
+    double third(double force);
+    double gravity(double mass_a, double mass_b, double distance);
+}
