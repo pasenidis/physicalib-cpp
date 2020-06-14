ce59c6affa67a0a7e61572c055db1f75124d3f71
Edward Pasenidis <me@edpasenidis.tech>
2020-06-15 02:37:41 +0300

    refractor: enum instead of if/else

--- src/newton.cpp
+++ src/newton.cpp
@@ -3,15 +3,19 @@
 #include "constants.hpp"
 #include "math.hpp"
 
-double LibPhysica::femt(double mass, std::string planet) {
-    if (planet == "Earth") {
-        return mass * 9.81;
-    } else if (planet == "Mars") {
-        return mass * 3.71;
-    } else if (planet == "Moon") {
-        return mass * 1.62;
-    } else {
-        return mass;
+
+double
+LibPhysica::femt(double mass, std::string planet) {
+    switch (LibPhysica::Object) {
+        case LibPhysica::Object::Earth:
+            return mass * 9.81;
+            break;
+        case LibPhysica::Object::Mars:
+            return mass * 3.71;
+            break;
+        case LibPhysica::Object::Moon:
+            return mass * 1.62;
+            break;
     }
 }
 
@@ -19,6 +23,6 @@ double LibPhysica::third(double force) {
     return force * -1;
 }
 
-double LibPhysica::gravity(double mass_a, double mass_b, double distance)
-    [return ((LibPhysica::BIG_G * mass_a * mass_b) / LibPhysica::pow(distance, 2));
-]
+double LibPhysica::gravity(double mass_a, double mass_b, double distance) {
+    return ((LibPhysica::BIG_G * mass_a * mass_b) / LibPhysica::pow(distance, 2));
+}
--- src/newton.hpp
+++ src/newton.hpp
@@ -2,6 +2,11 @@
 #include <string>
 
 namespace LibPhysica {
+    enum class Object {
+        Earth,
+        Mars,
+        Moon
+    }
     double femt(double mass, std::string planet);
     double third(double force);
     double gravity(double mass_a, double mass_b, double distance);
